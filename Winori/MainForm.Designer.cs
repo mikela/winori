615dcf25d2d405a6054f0b13aaa1ec182f22b291
mikela <mikelanabitarte@gmail.com>
2015-01-18 23:54:39 +0100

    First commit

---
+++ Winori/MainForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Winori
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.mouseKeyEventProvider1 = new MouseKeyboardActivityMonitor.Controls.MouseKeyEventProvider();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBoxCC = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCC)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mouseKeyEventProvider1
+            // 
+            this.mouseKeyEventProvider1.Enabled = false;
+            this.mouseKeyEventProvider1.HookType = MouseKeyboardActivityMonitor.Controls.HookType.Global;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Winori v1.0 \r\nby Mikel Anabitarte";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.BalloonTipText = "Type QQ, WW, AA, or SS to move windows around.";
+            this.notifyIcon.BalloonTipTitle = "Winori";
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Winori";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "and others";
+            this.toolTip1.SetToolTip(this.label2, resources.GetString("label2.ToolTip"));
+            // 
+            // pictureBoxCC
+            // 
+            this.pictureBoxCC.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxCC.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxCC.Image")));
+            this.pictureBoxCC.Location = new System.Drawing.Point(112, 16);
+            this.pictureBoxCC.Name = "pictureBoxCC";
+            this.pictureBoxCC.Size = new System.Drawing.Size(89, 32);
+            this.pictureBoxCC.TabIndex = 2;
+            this.pictureBoxCC.TabStop = false;
+            this.toolTip1.SetToolTip(this.pictureBoxCC, "CC BY SA");
+            this.pictureBoxCC.Click += new System.EventHandler(this.pictureBoxCC_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(212, 63);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBoxCC);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Winori";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCC)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MouseKeyboardActivityMonitor.Controls.MouseKeyEventProvider mouseKeyEventProvider1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBoxCC;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Winori/MainForm.cs
@@ -0,0 +1,103 @@
+﻿using MouseKeyboardActivityMonitor;
+using MouseKeyboardActivityMonitor.WinApi;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winori
+{
+    public partial class MainForm : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        private KeyboardHookListener m_KeyboardHookManager;
+
+        private char preKeyDown = ' ';
+        private int hw;
+        private int hh;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            m_KeyboardHookManager = new KeyboardHookListener(new GlobalHooker());
+            m_KeyboardHookManager.Enabled = true;
+            m_KeyboardHookManager.KeyPress += HookManager_KeyPress;
+
+            Rectangle screen = Screen.FromControl(this).WorkingArea;
+            hw = screen.Width / 2;
+            hh = screen.Height / 2;
+
+            this.WindowState = FormWindowState.Minimized;
+        }
+        
+        private void HookManager_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // QQ: move up left
+            if (preKeyDown == 'Q' && e.KeyChar == 'Q')
+            {
+                preKeyDown = ' ';
+                moveWindow(0, 0);
+            }
+            // WW: move up right
+            else if (preKeyDown == 'W' && e.KeyChar == 'W')
+            {
+                preKeyDown = ' ';
+                moveWindow(hw, 0);
+            }
+            // AA: move down left
+            else if (preKeyDown == 'A' && e.KeyChar == 'A')
+            {
+                preKeyDown = ' ';
+                moveWindow(0, hh);
+            }
+            // SS: move down right
+            else if (preKeyDown == 'S' && e.KeyChar == 'S')
+            {
+                preKeyDown = ' ';
+                moveWindow(hw, hh);
+            }
+            else
+            {
+                preKeyDown = e.KeyChar;
+            }            
+        }
+        
+        private void moveWindow(int x, int y)
+        {
+            MoveWindow(GetForegroundWindow(), x, y, hw, hh, true);
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon.ShowBalloonTip(3000);
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+        }
+
+        private void pictureBoxCC_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://creativecommons.org/licenses/by-sa/4.0/");
+        }
+
+    }
+}
